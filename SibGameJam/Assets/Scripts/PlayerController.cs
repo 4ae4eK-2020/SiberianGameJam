61c2633cf7b0fe0052fbd53c378f2b2b1f3a2207
SavageVictor <victor.v.oliinyk@gmail.com>
2021-11-06 16:40:56 +0200

    pushed

--- SibGameJam/Assets/Scripts/PlayerController.cs
+++ SibGameJam/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector3 center;
     [SerializeField] private float impulseMagnitude, speed;
 
+    [SerializeField] private PowerInput powerInput;
+
     private void Update()
     {
         playerRb.AddForce(Vector3.forward * speed, ForceMode.VelocityChange);
-        if(Input.GetKeyDown(KeyCode.Space)) playerRb.AddForceAtPosition(Vector3.up * impulseMagnitude, playerRb.position + center, ForceMode.Acceleration);
+        //if(Input.GetKeyDown(KeyCode.Space)) playerRb.AddForceAtPosition(Vector3.up * impulseMagnitude, playerRb.position + center, ForceMode.Acceleration);
+    }
+
+    public void Acceleration()
+    {
+        playerRb.AddForceAtPosition(Vector3.up * impulseMagnitude * powerInput.publicPower, playerRb.position + center, ForceMode.Acceleration);
     }
 }
---
+++ SibGameJam/Assets/Scripts/PointController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour
+{
+    [SerializeField] private int Points;
+    [SerializeField] private Text score;
+    
+    
+    private float time;
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > 1)
+        {
+            Points += 10;
+            time = 0;
+            score.text = Points.ToString();
+        }
+    }
+}
