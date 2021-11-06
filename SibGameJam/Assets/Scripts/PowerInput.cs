168d5a9b365aa1a0bb6b638f329e0a4f412e643f
4ae4eKru <78693525+4ae4eKru@users.noreply.github.com>
2021-11-06 19:09:03 +0500

    GGG

---
+++ SibGameJam/Assets/Scripts/PowerInput.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PowerInput : MonoBehaviour
+{
+    [SerializeField] private float power;
+    [SerializeField] public float publicPower;
+    
+    [SerializeField] private PlayerController playerController;
+
+    [SerializeField] private Slider powerBar;
+
+    private float Timer;
+
+    public void Start()
+    {
+        publicPower = 1;
+        playerController.Acceleration();
+        publicPower = 0;
+    }
+
+    public void Update()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer > 0.4)
+        {
+            if (Input.GetKey("space"))
+            {
+                power += Time.deltaTime * 2;
+            }
+            else if (Input.GetKeyUp("space"))
+            {
+                if (power > 1) power = 1;
+                publicPower = power;
+                playerController.Acceleration();
+                power = 0;
+                Timer = 0;
+            }
+        }
+        powerBar.value = power;
+    }
+}
