11079c43693936f0359029ec658a95b8546f0fd3
4ae4eKru <78693525+4ae4eKru@users.noreply.github.com>
2021-11-06 19:01:46 +0500

    FFF

--- SibGameJam/Assets/Scripts/LevelGenerator.cs
+++ SibGameJam/Assets/Scripts/LevelGenerator.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private Rigidbody playerRb;
     [SerializeField] private List<GameObject> prefabs;
     [SerializeField] private int step;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float direction;
     private int currentGameObject;
 
     void Start()
@@ -19,17 +23,21 @@ public class LevelGenerator : MonoBehaviour
     {
         while (true)
         {
-            int value = ((int)transform.position.z + 50) % step;
-            if (value == 0)
+            int result;
+            Math.DivRem((int)transform.position.z, step, out result);
+            if (result == 0)
             {
-                
+                Debug.Log("Generate object");
                 currentGameObject = Random.Range(0, prefabs.Count);
-                GameObject clone = Instantiate(prefabs[currentGameObject], new Vector3(0, 15.5f + prefabs[currentGameObject].transform.lossyScale.y/2,transform.position.z + 50 + step), Quaternion.Euler(Vector3.zero));
+                GameObject clone = Instantiate(prefabs[currentGameObject], new Vector3(offset.x, offset.y + prefabs[currentGameObject].transform.lossyScale.y/2,transform.position.z + offset.z + step), Quaternion.Euler(Vector3.zero));
+                Debug.Log(clone.name + " was generated at: X=" + clone.transform.position.x + ", Y=" + clone.transform.position.y + ", Z=" + clone.transform.position.z);
             }
-            if(playerRb.velocity.z >= 1) yield return new WaitForSeconds(2/playerRb.velocity.z);
+
+            Debug.Log(result);
+            if(playerRb.velocity.z >= direction) yield return new WaitForSeconds(direction/playerRb.velocity.z);
             else
             {
-                yield return new WaitForSeconds(0.2f);
+                yield return new WaitForSeconds(0.1f * direction);
             }
         }
     }
